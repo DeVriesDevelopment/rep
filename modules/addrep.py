a0057fb5c121182b099ff7ee8231ccccf217599d
Bart de Vries <bartdevries@Barts-MBP-2.dynamic.ziggo.nl>
2018-03-18 23:53:37 +0100

    test

--- modules/addrep.py
+++ modules/addrep.py
@@ -1,5 +1,9 @@
 from lib.firebase_database import *
+import discord
+import operator
 
+ranks = {0: "Crew Member", 15:"Buccaneers", 50:"Quartermasters", 100: "First Mates", 150: "Captains"}
+forRanks = sorted(ranks.items(), key=operator.itemgetter(0))
 async def addrep(message, client):
     author = message.author
     if "pirate lords" in [y.name.lower() for y in author.roles] or "captains" in [y.name.lower() for y in author.roles]:
@@ -10,6 +14,7 @@ async def addrep(message, client):
         if(len(addstring) < 3):
             await client.send_message(message.channel, "You didn't add enough arguments")
         user = message.mentions[0].id
+        realUser = message.mentions[0]
         amount = addstring[2]
         if(int(amount) < 500):
             rep = firebase_get("rep/" + user)
@@ -17,6 +22,17 @@ async def addrep(message, client):
                 firebase_update("rep/" + user, {'rep':amount})
             else:
                 firebase_update("rep/" + user, {'rep': int(amount) + int(rep['rep'])})
+
+            rep = firebase_get("rep/" + user)
+            rep = int(rep['rep'])
+            currentRank = 0
+            for rank in forRanks:
+                if(rank[0] <= rep):
+                    currentRank = rank[0]
+            for tmpRole in message.server.roles:
+                if(tmpRole.name == ranks.get(currentRank)):
+                    role = tmpRole
+            await client.add_roles(realUser, role)
             await client.send_message(message.channel, "Reputation has succesfully been added")
         else:
             await client.send_message(message.channel, "The amount is too high")
--- modules/subrep.py
+++ modules/subrep.py
@@ -1,20 +1,22 @@
 from lib.firebase_database import *
 
 async def subrep(message, client):
-    addstring = message.content.split()
-    if(len(addstring) > 3):
-        await client.send_message(message.channel, "You added too many arguments")
+    author = message.author
+    if "pirate lords" in [y.name.lower() for y in author.roles] or "captains" in [y.name.lower() for y in author.roles]:
+        addstring = message.content.split()
+        if(len(addstring) > 3):
+            await client.send_message(message.channel, "You added too many arguments")
 
-    if(len(addstring) < 3):
-        await client.send_message(message.channel, "You didn't add enough arguments")
-    user = message.mentions[0].id
-    amount = addstring[2]
-    if(int(amount) < 500):
-        rep = firebase_get("rep/" + user)
-        if(rep is None):
-            await client.send_message(message.channel, "This user has no reputation")
+        if(len(addstring) < 3):
+            await client.send_message(message.channel, "You didn't add enough arguments")
+        user = message.mentions[0].id
+        amount = addstring[2]
+        if(int(amount) < 500):
+            rep = firebase_get("rep/" + user)
+            if(rep is None):
+                await client.send_message(message.channel, "This user has no reputation")
+            else:
+                firebase_update("rep/" + user, {'rep':int(rep['rep']) - int(amount)})
+            await client.send_message(message.channel, "Reputation has succesfully been subtracted")
         else:
-            firebase_update("rep/" + user, {'rep':int(rep['rep']) - int(amount)})
-        await client.send_message(message.channel, "Reputation has succesfully been subtracted")
-    else:
-        await client.send_message(message.channel, "The amount is too high")
+            await client.send_message(message.channel, "The amount is too high")
